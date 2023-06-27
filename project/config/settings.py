ba2c0860484a6f9dc56590838f87ec75f5c6bc4c
mike emmanuel <111892903+ikemanuuel@users.noreply.github.com>
2023-06-27 14:23:59 +0800

    Update settings.py

--- project/config/settings.py
+++ project/config/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'corsheaders',
-    'accounts',
-    'residents'
 
 
 ]
@@ -134,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
